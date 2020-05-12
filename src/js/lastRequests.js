b103d6b97a59681e720766c9cf17f4c8744f6a72
Vitsk <vitya23052@gmail.com>
2020-05-12 10:54:22 +0300

    "Fixed some bugs"

--- src/js/lastRequests.js
+++ src/js/lastRequests.js
@@ -1,21 +1,25 @@
 export default function renderRequests(content) {
-  const requestOutput = document.querySelector('.requests-output');
+  const requestOutput = document.querySelector('.requests');
   let html = `
-    <div class="requests-last">
-      <a class="request-listener">${content[0] == undefined ? '' : content[0]}</a>
-    </div>
-    <div class="requests-last">
-      <a class="request-listener">${content[1] == undefined ? '' : content[1]}</a>
-    </div>
-    <div class="requests-last">
-      <a class="request-listener">${content[2] == undefined ? '' : content[2]}</a>
-    </div>
-    <div class="requests-last">
-      <a class="request-listener">${content[3] == undefined ? '' : content[3]}</a>
-    </div>
-    <div class="requests-last">
-      <a class="request-listener">${content[4] == undefined ? '' : content[4]}</a>
-    </div>
+    <h1 class="requests-header">${content[0] ? 'Last requests' : ''}</h1>
+      <div class="requests-output">
+        <div class="requests-last">
+          <a class="request-listener">${content[0] == undefined ? '' : content[0]}</a>
+        </div>
+        <div class="requests-last">
+          <a class="request-listener">${content[1] == undefined ? '' : content[1]}</a>
+        </div>
+        <div class="requests-last">
+          <a class="request-listener">${content[2] == undefined ? '' : content[2]}</a>
+        </div>
+        <div class="requests-last">
+          <a class="request-listener">${content[3] == undefined ? '' : content[3]}</a>
+        </div>
+        <div class="requests-last">
+          <a class="request-listener">${content[4] == undefined ? '' : content[4]}</a>
+        </div>
+      </div>
+    
   `;
 
   requestOutput.innerHTML = html;
